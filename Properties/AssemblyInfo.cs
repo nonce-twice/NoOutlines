26e473adfdf842a8065bf2b9a1d58625bd9dec14
nonce-twice <>
2021-03-05 12:22:33 -0800

    Update version number to 0.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
-[assembly: MelonInfo(typeof(NoOutlines.NoOutlines), "No Outlines", "0.1", "nonce-twice")]
+[assembly: MelonInfo(typeof(NoOutlines.NoOutlines), "No Outlines", "0.2", "nonce-twice")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace NoOutlines
@@ -8,7 +8,7 @@ namespace NoOutlines
     {
         public const string Name = "No Outlines";
         public const string Author = "nonce-twice";
-        public const string Version = "0.1";
+        public const string Version = "0.2";
         public const string DownloadLink = "https://github.com/nonce-twice/NoOutlines";
     }
 }
